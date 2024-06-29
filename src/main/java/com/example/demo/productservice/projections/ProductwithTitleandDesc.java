731443b655dc297923cb0d8c997339abca313c3c
Avi-000-Avi <atoppo267@gmail.com>
2024-06-29 20:08:54 +0530

    fix(portal): fix

---
+++ src/main/java/com/example/demo/productservice/projections/ProductwithTitleandDesc.java
@@ -0,0 +1,12 @@
+package com.example.demo.productservice.projections;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductwithTitleandDesc {
+
+    private String title;
+    private String description;
+}
--- src/main/java/com/example/demo/productservice/repos/ProductRepo.java
+++ src/main/java/com/example/demo/productservice/repos/ProductRepo.java
@@ -2,7 +2,10 @@ package com.example.demo.productservice.repos;
 
 
 import com.example.demo.productservice.models.Product;
+import com.example.demo.productservice.projections.ProductwithTitleandDesc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -18,4 +21,11 @@ public interface ProductRepo extends JpaRepository<Product, Long> {
     @Override
     Product save(Product product); //  create and update
 
+    //HQL
+    @Query("select p.title,p.description from Product  p where p.id=:id")
+    ProductwithTitleandDesc someRandomQuery(@Param("id") Long id);
+
+    //SQL
+    @Query(value = "select title,description from product where id = :id",nativeQuery = true)
+    ProductwithTitleandDesc someRandomQuery(@Param("id") Long id);
 }
